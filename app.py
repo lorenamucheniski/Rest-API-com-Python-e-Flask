d00bd6147decdef743dbbb1211ed24cf567d6e61
lorenamucheniski <lomucheniski25@gmail.com>
2022-11-15 16:22:55 -0300

    api completa

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify
+import json
+
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -10,9 +12,16 @@ desenvolvedores = [
      'Habilidades': ['Python', 'Django']}
 ]
 
-@app.route('/dev')
-def desenvolvedor():
-    return jsonify({'nome':'Lorena'})
+@app.route('/dev/<int:id>/', methods = ['GET' , 'PUT'])
+def desenvolvedor(id):
+    if request.method == 'GET':
+        desenvolvedor = desenvolvedores[id]
+        print(desenvolvedor)
+        return jsonify(desenvolvedor)
+    elif request.method == 'PUT':
+        dados = json.loads(request.data)
+        desenvolvedores[id] = dados
+        return jsonify(dados)
 
 if __name__ == '__main__':
     app.run(debug = True)
