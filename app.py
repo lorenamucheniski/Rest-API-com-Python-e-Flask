75e6e4f0b006fae6d49fbd3efaf5361bf61f825c
lorenamucheniski <lomucheniski25@gmail.com>
2022-11-15 17:03:59 -0300

    API completa

--- app.py
+++ app.py
@@ -5,23 +5,46 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 desenvolvedores = [
-    {'Nome': 'Lorena',
-     'Habilidades': ['Python', 'Flask']
+    {   'id' : '0',
+        'Nome': 'Lorena',
+        'Habilidades': ['Python', 'Flask']
      },
-    {'Nome': 'Diego',
+    {   'id':'1',
+        'Nome': 'Diego',
      'Habilidades': ['Python', 'Django']}
 ]
 
-@app.route('/dev/<int:id>/', methods = ['GET' , 'PUT'])
+#devolver um desenvolvedor pelo ID, fazer alterações e deletar
+@app.route('/dev/<int:id>/', methods = ['GET' , 'PUT', 'DELETE'])
 def desenvolvedor(id):
     if request.method == 'GET':
-        desenvolvedor = desenvolvedores[id]
-        print(desenvolvedor)
-        return jsonify(desenvolvedor)
+        try:
+            response = desenvolvedores[id]
+        except IndexError:
+            mensagem = f'Desenvolvedor de id {id} não existe'
+            response = {'status':'erro', 'mensagem': mensagem}
+        except Exception:
+            mensagem = 'Erro desconhecido. Procure o administrador da API'
+            response = {'status':'erro', 'mensagem': mensagem}
+        return jsonify(response)
     elif request.method == 'PUT':
         dados = json.loads(request.data)
         desenvolvedores[id] = dados
         return jsonify(dados)
+    elif request.method == 'DELETE':
+        desenvolvedores.pop(id)
+        return jsonify({'status':'Sucesso', 'mensagem': 'Registo excluído!'})
+
+# lista todos os desenvolvedores e inclui novos, permite registrar
+@app.route('/dev/', methods = ['POST', 'GET'])
+def lista_desenvolvedores():
+    if request.method == 'POST':
+        dados = json.loads(request.data)
+        posicao = len(desenvolvedores)
+        dados['id'] = posicao
+        desenvolvedores.append(dados)
+        return jsonify(desenvolvedores[posicao])
+
 
 if __name__ == '__main__':
     app.run(debug = True)
