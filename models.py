4f727ed01fd903267721b5bc8538ffe1152e3a3c
lorenamucheniski <lomucheniski25@gmail.com>
2022-12-03 16:19:02 -0300

    manipulando banco de dados com SQLALchemy

--- models.py
+++ models.py
@@ -1,3 +1,11 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.txt.declarative import declarative_base
 
-engine = create_engine ('sqlite: ///atividades.db')
+engine = create_engine ('sqlite: ///atividades.db', convert_unicode = True)
+db_session = scoped_session(sessionmaker(autocommit = False, binds = engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+
