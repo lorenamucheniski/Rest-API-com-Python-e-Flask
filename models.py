17a912984b60c9b7b0e86faf67437ea2d168be35
lorenamucheniski <lomucheniski25@gmail.com>
2022-12-03 16:30:10 -0300

    manipulando banco de dadoss com SQLALchemy

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.txt.declarative import declarative_base
 
@@ -8,4 +8,17 @@ db_session = scoped_session(sessionmaker(autocommit = False, binds = engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
 
+class Pessoas(Base):
+    __tablename__ = 'pessoas'
+    id = Column(Integer, primary_key = True)
+    nome = Column(String(40), index = True)
+    idade = Column(Integer)
 
+    def __repr__(self):
+        return '<Pessoa {}>'.format(self.nome)
+
+class Atividades(Base):
+    __tablename__ = 'atividades'
+    id = Column(Integer, primary_key = True)
+    nome = Column(String(80))
+    
